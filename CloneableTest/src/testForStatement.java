236564caf7cc4bd761dedad2b40fa50aaa3d37da
bianbian614 <bianbian614@gmail.com>
2015-03-25 19:43:27 +0800

    commit

---
+++ CloneableTest/src/testForStatement.java
@@ -0,0 +1,12 @@
+/**
+ * Created by canglangwenyue on 15-1-1.
+ */
+public class testForStatement {
+
+    public static void main(String[] args) {
+        String[] mList = {"123455","dew"};
+        for(String anInt: mList) {
+            System.out.println(mList.toString().getBytes());
+        }
+    }
+}
